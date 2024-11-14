ce92730a14d4541727db9734678bdf0298877cbd
Rohan Kathuria <rohan.kathuria@live.com>
2024-11-14 13:47:59 -0800

    updated some configs

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,6 @@ if status is-interactive
     set fish_greeting
 end
 source /Users/rohan/venv/bin/activate.fish
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /Users/rohan/.cache/lm-studio/bin
